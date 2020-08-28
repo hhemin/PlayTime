42c9e0bfc6999fe02b51cc5503d9104748f103a2
hhemin <heminfind@aliyun.com>
2020-08-29 01:19:27 +0800

    文件整理

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const Koa = require('koa')
 const app = new Koa();
 const convert = require('koa-convert')
-const cors = require('koa2-cors');
+const cors = require('koa-cors');
 const parser = require('koa-bodyparser');
 const InitManager = require('./core/init');
 const catchError = require('./middlewares/exception'); // 捕获错误的中间件
 
-app.use(cors())
+app.use(convert(cors()))
 app.use(catchError);
 app.use(parser());
 InitManager.initCore(app);
